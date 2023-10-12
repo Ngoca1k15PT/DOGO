ff4a132ab60d6f5d7588140fc876d4ad7453ff6f
DinhHoangNgoc <ngocdh@flydinotech.com>
2023-10-12 17:18:39 +0700

    add library livestream

--- src/components/live/LiveStreamScreen.tsx
+++ src/components/live/LiveStreamScreen.tsx
@@ -1,14 +1,14 @@
 import * as React from 'react';
 import {Text, View, StyleSheet} from 'react-native';
+import IVSPlayer from 'amazon-ivs-react-native-player';
 
 interface componentNameProps {}
 
 const LiveStreamScreen = (props: componentNameProps) => {
-  return (
-    <View style={styles.container}>
-      <Text>componentName</Text>
-    </View>
-  );
+  const URL =
+    'https://fcc3ddae59ed.us-west-2.playback.live-video.net/api/video/v1/us-west-2.893648527354.channel.DmumNckWFTqz.m3u8';
+
+  return <IVSPlayer streamUrl={URL} />;
 };
 
 export default LiveStreamScreen;
